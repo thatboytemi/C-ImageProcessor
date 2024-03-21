6e20d541cf669642a9c89c9a5910e4d518bc149c
Temi <temi0072@gmail.com>
2024-03-21 18:15:11 +0200

    Added function declarations for image processor class

---
+++ PGMimageProcessor.h
@@ -0,0 +1,74 @@
+#include <string>
+#include "ConnectedComponent.h"
+#include <memory>
+
+//will be used for templated code
+typedef unsigned char Gray;
+struct Colour{
+    unsigned char pixels[3];
+};
+
+template <typename T> class PGMimageProcessor{
+    private:
+        int min;
+        int max;
+        int imageWidth =0;
+        int imageHeight =0;
+        unsigned char threshold;
+        //identifier not used for PGMimageProcessor object but for components within a PGMimageProcessor class
+        int identifier =0;
+        std::string filename;
+        std::vector<std::unique_ptr<ConnectedComponent>> components;
+    public:
+        //Constructors
+        PGMimageProcessor();
+        PGMimageProcessor(const PGMimageProcessor & other);
+        PGMimageProcessor(PGMimageProcessor && other);
+        PGMimageProcessor & operator=(const PGMimageProcessor & rhs);
+        PGMimageProcessor & operator=(PGMimageProcessor && rhs);
+        PGMimageProcessor(int min, int max, unsigned char threshold, std::string filename, bool print, bool write, std::string outFileName);
+        ~PGMimageProcessor();
+        /* process the input image to extract all the connected components,
+        based on the supplied threshold (0...255) and excluding any components
+        of less than the minValidSize. The final number of components that
+        you store in your container (after discarding undersized one)
+        must be returned.
+        */
+
+        int extractComponents(unsigned char threshold, int minValidSize);
+
+        /* iterate - with an iterator - though your container of connected
+        components and filter out (remove) all the components which do not
+        obey the size criteria pass as arguments. The number remaining
+        after this operation should be returned.
+        */
+
+        int filterComponentsBySize(int minSize, int maxSize);
+
+        /* create a new PGM file which contains all current components
+        (255=component pixel, 0 otherwise) and write this to outFileName as a
+        valid PGM. the return value indicates success of operation
+        */
+
+        bool writeComponents(const std::string & outFileName);
+
+        // return number of components
+
+        int getComponentCount(void) const;
+        
+        // return number of pixels in largest component
+
+        int getLargestSize(void) const;
+
+        // return number of pixels in smallest component
+
+        int getSmallestSize(void) const;
+
+        /* print the data for a component to std::cout
+        see ConnectedComponent class;
+        print out to std::cout: component ID, number of pixels
+        */
+
+        void printComponentData(const ConnectedComponent & theComponent) const;
+
+};
